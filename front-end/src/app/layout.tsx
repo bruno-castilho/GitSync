e0c279cdedad3f9b6c9bf6ca33d029bd7d1c82d3
Bruno <bruno.silva.castilho@gmail.com>
2025-03-11 13:31:14 -0300

    created theme

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,7 +1,12 @@
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 import { ReactNode } from 'react'
 import { DefaultLayout } from '../layouts/DefaultLayout'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import { defaultTheme } from '@/theme'
 
 interface RootLayoutProps {
   children: ReactNode
@@ -11,9 +16,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-br">
       <body>
-        <AppRouterCacheProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <ThemeProvider theme={defaultTheme} >
           <CssBaseline />
           <DefaultLayout>{children}</DefaultLayout>
+        </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
---
+++ front-end/src/theme.ts
@@ -0,0 +1,11 @@
+'use client';
+import { createTheme } from '@mui/material/styles'
+
+export const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#143d59',
+      contrastText: '#fff',
+    },
+  },
+})

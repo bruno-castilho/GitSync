57419543d6cd05562bb17a00fed403b483d2f88f
Bruno <bruno.silva.castilho@gmail.com>
2025-03-16 15:56:54 -0300

    added theme

--- front-end/src/theme.ts
+++ front-end/src/theme.ts
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 import { createTheme } from '@mui/material/styles'
 
 export const defaultTheme = createTheme({
@@ -6,6 +6,24 @@ export const defaultTheme = createTheme({
     primary: {
       main: '#143d59',
       contrastText: '#fff',
+    },
+    secondary: {
+      main: '#f4b41a',
+      contrastText: '#fff',
+    },
+  },
+  colorSchemes: {
+    dark: {
+      palette: {
+        secondary: {
+          main: '#143d59',
+          contrastText: '#fff',
+        },
+        primary: {
+          main: '#f4b41a',
+          contrastText: '#fff',
+        },
+      },
     },
   },
 })
